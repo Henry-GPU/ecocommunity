9b0d9a162a55e9c5988b57e574c3023b1ce939a1
Henry-GPU <68120046+Henry-GPU@users.noreply.github.com>
2024-10-08 12:18:16 -0600

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,42 +1,50 @@
 import React from "react";
 import Post from './Post';
 
-function Feed({posts, userEmail}){
+function Feed({ posts, userEmail }) {
     return (
         <div>
-            {posts.map((post) => (
-                <Post
-                    post={post}
-                    key={post.Id} 
-                    userEmail={post.UserEmail}
-                    name={post.UserName}
-                    time={new Date(post.CreatedAt).toLocaleString(
-                        "es-GT", {
-                            timeZone: "America/Guatemala",
-                            month: "long",
-                            day: "numeric",
-                            hour: "numeric",
-                            minute: "2-digit",
-                            hour12: false,
-
-                    })}
-                    comment={post.Comment}
-                    postImage={post.PostImage}
-                    location={
-                        typeof post.Location === "string"
-                        ?   {
-                            lat: parseFloat(post.Location.split(",")[0]),
-                            lng: parseFloat(post.Location.split(",")[1]),
+            {Array.isArray(posts) && posts.length > 0 ? (
+                posts.map((post) => (
+                    <Post
+                        post={post}
+                        key={post.Id}
+                        userEmail={post.UserEmail}
+                        name={post.UserName}
+                        time={new Date(post.CreatedAt).toLocaleString(
+                            "es-GT",
+                            {
+                                timeZone: "America/Guatemala",
+                                month: "long",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "2-digit",
+                                hour12: false,
                             }
-                        :   {
-                            lat: post.Location.lat, 
-                            lng: post.Location.lng
-                            }
-                    }
-                    userAuthEmail={userEmail}
-                />
-            ))}
-        </div>     
+                        )}
+                        comment={post.Comment}
+                        postImage={post.PostImage}
+                        location={
+                            typeof post.Location === "string"
+                                ? {
+                                      lat: parseFloat(
+                                          post.Location.split(",")[0]
+                                      ),
+                                      lng: parseFloat(
+                                          post.Location.split(",")[1]
+                                      ),
+                                  }
+                                : {
+                                      lat: post.Location.lat,
+                                      lng: post.Location.lng,
+                                  }
+                        }
+                        userAuthEmail={userEmail}
+                    />
+                ))
+            ) : (
+                <p>No hay publicaciones disponibles</p>
+            )}
+        </div>
     );
 }
-export default Feed;
